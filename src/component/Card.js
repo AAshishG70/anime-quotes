386df3efb3818fc040d81043d45b3c5d4df1e9cc
Aashish Gubhaju <aashishgubhaju12@gmail.com>
2022-12-30 11:44:42 -0700

    add react-icons

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,14 +1,22 @@
+import { MdMovieFilter } from "react-icons/md";
+import { GiCharacter } from "react-icons/gi";
+import { BsChatRightQuoteFill } from "react-icons/bs";
+import "./Card.css";
+
 function Card({ animeDetails }) {
   const { anime, character, quote } = animeDetails;
   // const anime = animeDetails.anime;
   // const character = animeDetails.character;
   // const quote = animeDetails.quote;
   return (
-    <div>
+    <div className="container">
+      <MdMovieFilter />
       {anime}
       <br />
+      <GiCharacter />
       {character}
       <br />
+      <BsChatRightQuoteFill />
       {quote}
     </div>
   );
